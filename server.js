ce0c4bf22cd689bccf92e4f5e8096ae24b11eb23
elroi773 <s2457@e-mirim.hs.kr>
2025-04-29 09:10:23 +0900

    하는중!~

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const getRandomInitials = require('./getRandomInitials');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public')); // public 폴더에 HTML 파일
+
 // 랜덤 초성 쌍 조회 API
 app.get('/random-initials', (req, res) => {
   getRandomInitials((randomInitials) => {
-    res.json(randomInitials); // 랜덤 초성 쌍 반환
+    res.json(randomInitials);
   });
 });
 
